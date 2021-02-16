74afdaab5f7bcc3e7283556546f728566cee08f6
syedammarilyas111 <syedammarilyas@hotmail.com>
2021-02-17 00:51:21 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+  
+   <div className="app_body">
+     {/* Sidebar */}
+     {/* Chat */}
+   </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAegHmrMHRjLkUBrEarLpjbvZX6FAlDYPQ",
+    authDomain: "pmd-chat.firebaseapp.com",
+    projectId: "pmd-chat",
+    storageBucket: "pmd-chat.appspot.com",
+    messagingSenderId: "873412832333",
+    appId: "1:873412832333:web:04af27b234b0d7e5ac4004",
+    measurementId: "G-442BZWVN05"
+  };
